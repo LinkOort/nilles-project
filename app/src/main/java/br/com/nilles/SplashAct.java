ef68a1e9f725e9fb70b05ea3d852b16dd376cbdf
Davi de Oliveira Santos <davi.santos@123etec.sp.gov.br>
2019-08-26 17:51:22 -0300

    SplashACT was created

--- app/src/main/java/br/com/nilles/SplashAct.java
+++ app/src/main/java/br/com/nilles/SplashAct.java
@@ -1,13 +1,34 @@
 package br.com.nilles;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashAct extends AppCompatActivity {
+
+
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashAct.this, MainActivity.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        },SPLASH_SCREEN_TIME_OUT);
     }
 }
