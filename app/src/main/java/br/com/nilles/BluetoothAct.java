f00f37a0d26d330c397d03fcdb787394e6a0d36f
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-09-23 19:22:03 -0300

    Bluetooth was added

--- app/src/main/java/br/com/nilles/BluetoothAct.java
+++ app/src/main/java/br/com/nilles/BluetoothAct.java
@@ -1,27 +1,124 @@
 package br.com.nilles;
 
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class BluetoothAct extends AppCompatActivity {
+public class Bluetooth_Activity extends AppCompatActivity {
+
+
+    public static int ENABLE_BLUETOOTH = 1;
+    static TextView statusMessage;
+    static TextView counterMessage;
+    ConnectionThread connect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.support_frag);
 
-        Button button = (Button)findViewById(R.id.btnBluetooth);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (btAdapter == null) {
+            statusMessage.setText("Que pena! Hardware Bluetooth não está funcionando :(");
+        } else {
+            statusMessage.setText("Ótimo! Hardware Bluetooth está funcionando :)");
+        }
 
-           //just a test
+        /* A chamada do seguinte método liga o Bluetooth no dispositivo Android
+            sem pedido de autorização do usuário. É altamente não recomendado no
+            Android Developers, mas, para simplificar este app, que é um demo,
+            faremos isso. Na prática, em um app que vai ser usado por outras
+            pessoas, não faça isso.
+         */
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH);
+            Toast.makeText(this, "Solicitando ativação do Bluetooth...", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Bluetooth já ativado :)", Toast.LENGTH_SHORT).show();
+        }
 
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        /* Definição da thread de conexão como cliente.
+            Aqui, você deve incluir o endereço MAC do seu módulo Bluetooth.
+            O app iniciará e vai automaticamente buscar por esse endereço.
+            Caso não encontre, dirá que houve um erro de conexão.
+         */
+        connect = new ConnectionThread("98:D3:36:F5:9E:F4");
+        connect.start();
+
+        /* Um descanso rápido, para evitar bugs esquisitos.
+         */
+        try {
+            Thread.sleep(1000);
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
     }
-}
+    
+    @SuppressLint("HandlerLeak")
+    public static Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+
+            /* Esse método é invocado na Activity principal
+                sempre que a thread de conexão Bluetooth recebe
+                uma mensagem.
+             */
+            Bundle bundle = msg.getData();
+            byte[] data = bundle.getByteArray("data");
+            String dataString = new String(data);
+
+            /* Aqui ocorre a decisão de ação, baseada na string
+                recebida. Caso a string corresponda à uma das
+                mensagens de status de conexão (iniciadas com --),
+                atualizamos o status da conexão conforme o código.
+             */
+            if (dataString.equals("---N"))
+                statusMessage.setText("Ocorreu um erro durante a conexão D:");
+            else if (dataString.equals("---S"))
+                statusMessage.setText("Conectado :D");
+            else {
+
+                /* Se a mensagem não for um código de status,
+                    então ela deve ser tratada pelo aplicativo
+                    como uma mensagem vinda diretamente do outro
+                    lado da conexão. Nesse caso, simplesmente
+                    atualizamos o valor contido no TextView do
+                    contador.
+                 */
+                counterMessage.setText(dataString);
+            }
+
+        }
+    };
+
+    /* Esse método é invocado sempre que o usuário clicar na TextView
+        que contem o contador. O app Android transmite a string "restart",
+        seguido de uma quebra de linha, que é o indicador de fim de mensagem.
+     */
+    public void restartCounter(View view) {
+        connect.write("restart\n".getBytes());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == ENABLE_BLUETOOTH) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Bluetooth já ativado :)", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                Toast.makeText(this, "Bluetooth não ativado :(", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
