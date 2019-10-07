e7ff310fda29959b690a0fa3e4dc3c5c4a1fcd8b
Davi de Oliveira <davi.santos123@etec.sp.gov.br>
2019-10-07 19:48:51 -0300

    The permissionact was added and u need fix cuz when the user confirm the permission that windown should disappear

--- app/src/main/java/br/com/nilles/PermissionAct.java
+++ app/src/main/java/br/com/nilles/PermissionAct.java
@@ -1,13 +1,77 @@
 package br.com.nilles;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class PermissionAct extends AppCompatActivity {
+
+    private Button btnAccept;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.permission_layout);
+
+
+        btnAccept = (Button)findViewById(R.id.accept);
+        btnAccept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withActivity(PermissionAct.this)
+                        .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse response) {
+                                startActivity(new Intent(PermissionAct.this, MainActivity.class));
+                                finish();
+                                return;
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse response) {
+                                if (response.isPermanentlyDenied()){
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(PermissionAct.this);
+                                    builder.setTitle("Permissão Negada")
+                                            .setMessage("Permissão para acessar a localização foi negada. Você necessita aceitar par utilizar aplicação")
+                                            .setNegativeButton("Cancelar",null)
+                                            .setPositiveButton("Aceitar", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    Intent intent = new Intent();
+                                                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                                    intent.setData(Uri.fromParts("package", getPackageName(), null));
+                                                }
+                                            })
+                                            .show();
+                                } else {
+                                    Toast.makeText(PermissionAct.this,"Permissão Negada", Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                                token.continuePermissionRequest();
+                            }
+                        })
+                        .check();
+            }
+        });
     }
 }
